60812b7165eeac29500e72c1694b205288731697
Sreya <sreyacthomas@gmail.com>
2026-03-10 10:31:46 +0530

    4

---
+++ Documents/RA2411003012285/PalindromeCheckerApp/App/src/PalindromeCheckerApp.java
@@ -0,0 +1,80 @@
+/**
+ * ==========================================================
+ * MAIN CLASS – UseCase1PalindromeCheckerApp
+ * ==========================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No palindrome logic is implemented yet.
+ *
+ * The goal is to establish a clear startup flow.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    private static final String APP_NAME = "Palindrome Checker Management System";
+    private static final String VERSION = "1.0";
+
+    /**
+     * Application entry point.
+     *
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        displayWelcomeMessage();
+        String input = "radar";
+
+        // Convert the string into a character array
+        char[] chars = input.toCharArray();
+
+        // Initialize pointers
+        int start = 0;
+        int end = chars.length - 1;
+
+        // Assume palindrome initially
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    /**
+     * Displays applicatio
+     * n welcome message and version.
+     */
+    private static void displayWelcomeMessage() {
+        System.out.println("===============================================");
+        System.out.println(" Welcome to the " + APP_NAME);
+        System.out.println(" Version: " + VERSION);
+        System.out.println("===============================================");
+        System.out.println("Application started successfully.");
+    }
+
+}
